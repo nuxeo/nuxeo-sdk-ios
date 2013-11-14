15767ec9f54ceb48f40ff1b13ff9bdd70cc5d0e5
Arnaud Kervern <akervern@nuxeo.com>
2013-11-14 15:59:22 +0100

    NXP-13170: Add script to prepare pod files and start tests

---
+++ build-and-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+HERE=$(cd $(dirname $0); pwd -P)
+NX_INT=$HERE/download/nuxeo-integration
+XCODEBUILD=${XCODEBUILD:-xcodebuild}
+
+# Cleaning
+rm -rf download nuxeo tomcat || exit 1
+
+# Cloning integration-release
+git clone https://github.com/nuxeo/integration-scripts $NX_INT
+
+# Loading integration-lib.sh
+. $NX_INT/integration-lib.sh
+
+$NX_INT/download.sh
+
+# Make wizard done.
+echo "nuxeo.wizard.done=true" >> tomcat/bin/nuxeo.conf
+# deploy nuxeo-rest-api
+echo "init" >> tomcat/nxserver/data/installAfterRestart.log
+echo "install nuxeo-rest-api" >> tomcat/nxserver/data/installAfterRestart.log
+start_server $HERE/tomcat 127.0.0.1
+
+# Prepare CocoaPod workspace
+./prepare-pod.sh
+
+echo "Build: `type $XCODEBUILD`"
+$XCODEBUILD -configuration Debug -sdk iphonesimulator -workspace NuxeoSDK/NuxeoSDK.xcworkspace -scheme NuxeoSDK clean test
+EXIT_CODE=$?
+
+stop_server
+
+exit $EXIT_CODE
